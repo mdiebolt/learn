ba70fce3ad4342cb9b2430fbd4f5834b6b1d3cbe
Matt Diebolt <mdiebolt@gmail.com>
2026-05-09 16:03:00 -0400

    Fix failing tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session
   resource :preferences
-  resources :passwords
+  resources :passwords, param: :token
   resources :registrations
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
